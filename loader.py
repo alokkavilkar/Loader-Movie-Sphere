6fece71b0e624be3baa42530b6f7b70ea08cba6b
alokkavilkar <alokkavilkar1057@gmail.com>
2024-07-13 20:34:38 +0530

    loader update

--- loader.py
+++ loader.py
@@ -11,14 +11,14 @@ load_dotenv()
 
 # SQS configuration
 sqs = boto3.client(
-    'sqs', 
-    region_name='us-east-1',               
+    'sqs',
+    region_name='us-east-1',
     aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
     aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
 
 ssm = boto3.client(
-    'ssm', 
-    region_name='us-east-1',                
+    'ssm',
+    region_name='us-east-1',
     aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
     aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
 
